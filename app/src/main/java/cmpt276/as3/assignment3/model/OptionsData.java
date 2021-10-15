fcc059496ac934e7d6b1e860c909eff6ef391445
Bry-D <bryandang10@gmail.com>
2021-10-15 07:52:06 -0700

    Create Singleton design for OptionsData class, and add setters and getters

--- app/src/main/java/cmpt276/as3/assignment3/model/OptionsData.java
+++ app/src/main/java/cmpt276/as3/assignment3/model/OptionsData.java
@@ -5,4 +5,37 @@ package cmpt276.as3.assignment3.model;
  * and to retrieve options before creating a new game.
  */
 public class OptionsData {
+    private int rowNum;
+    private int columnNum;
+    private int mineNum;
+
+    // Make OptionsData class into a Singleton
+    private static OptionsData instance;
+    private OptionsData() {
+        // Private to prevent anyone from instantiating
+    }
+
+    public int getRowNum() {
+        return rowNum;
+    }
+
+    public void setRowNum(int rowNum) {
+        this.rowNum = rowNum;
+    }
+
+    public int getColumnNum() {
+        return columnNum;
+    }
+
+    public void setColumnNum(int columnNum) {
+        this.columnNum = columnNum;
+    }
+
+    public int getMineNum() {
+        return mineNum;
+    }
+
+    public void setMineNum(int mineNum) {
+        this.mineNum = mineNum;
+    }
 }
