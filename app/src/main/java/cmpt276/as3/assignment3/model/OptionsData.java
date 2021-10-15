c8eb321952727bcf7c9fd76c1bd6b0dfff2f8097
Bry-D <bryandang10@gmail.com>
2021-10-14 22:23:03 -0700

    Create all Activities and initial model classes

---
+++ app/src/main/java/cmpt276/as3/assignment3/model/OptionsData.java
@@ -0,0 +1,8 @@
+package cmpt276.as3.assignment3.model;
+
+/**
+ * Class to store the options that the user sets before playing a game,
+ * and to retrieve options before creating a new game.
+ */
+public class OptionsData {
+}
