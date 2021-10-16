2623d93ce17b798bf01875c11e5831d10a665da5
Lynn Nguyen <nnn2@sfu.ca>
2021-10-16 13:29:52 -0700

    Update GameActivity.java with dynamic buttons.
    Add image to the buttons when user clicks on them.

--- app/src/main/java/cmpt276/as3/assignment3/GameActivity.java
+++ app/src/main/java/cmpt276/as3/assignment3/GameActivity.java
@@ -1,15 +1,29 @@
 package cmpt276.as3.assignment3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
 
 /**
  * Activity that displays the game
  */
 public class GameActivity extends AppCompatActivity {
+    private static final int NUM_ROWS = 6;
+    private static final int NUM_COLS = 15;
+    Button buttons[][] = new Button[NUM_ROWS][NUM_COLS];
 
     public static Intent gameLaunchIntent(Context c) {
         Intent intent = new Intent(c, GameActivity.class);
@@ -20,6 +34,78 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        populateButtons();
+    }
+
+    private void populateButtons() {
+        TableLayout table = (TableLayout) findViewById(R.id.tableForButtons);
+
+        for (int row = 0; row < NUM_ROWS; row++) {
+            TableRow tableRow = new TableRow(GameActivity.this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f));
+            table.addView(tableRow);
+
+            for (int col = 0; col < NUM_COLS; col++) {
+                final int FINAL_ROW = row;
+                final int FINAL_COL = col;
+
+                Button button = new Button(GameActivity.this);
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f));
+
+                button.setText(row + "," + col);
+                // Make text not clip on the small buttons.
+                button.setPadding(0,0,0,0);
+
+                // Display a message when accessing each button
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        gridButtonClicked(FINAL_ROW, FINAL_COL);
+                    }
+                });
+
+                tableRow.addView(button);
+                buttons[row][col] = button;
+            }
+        }
+    }
+
+    private void gridButtonClicked(int row, int col) {
+        Toast.makeText(GameActivity.this, "Button clicked" + row + ", " + col, Toast.LENGTH_SHORT)
+                .show();
+
+        // Display image after the button is clicked.
+        Button currentButton = buttons[row][col];
+
+        // Lock the button size
+        lockButtonSize();
+        currentButton.setBackgroundResource(R.drawable.ic_android_black_24dp);
+
+        // Change text after clicking.
+        currentButton.setText("CAT");
+    }
+
+    private void lockButtonSize() {
+        for (int row = 0; row < NUM_ROWS; row++) {
+            for (int col = 0; col < NUM_COLS; col++) {
+                Button currentButton = buttons[row][col];
+
+                int width = currentButton.getWidth();
+                currentButton.setMinWidth(width);
+                currentButton.setMaxWidth(width);
+
+                int height = currentButton.getHeight();
+                currentButton.setMinHeight(height);
+                currentButton.setMaxHeight(height);
+            }
+        }
     }
 
     // call getter method to create the game
