c7e8ce813b15c4cdd492a7bcf6fd29474d7f3a8a
Bry-D <bryandang10@gmail.com>
2021-10-16 16:23:57 -0700

    Add comments to OptionsActivity.java

--- app/src/main/java/cmpt276/as3/assignment3/OptionsActivity.java
+++ app/src/main/java/cmpt276/as3/assignment3/OptionsActivity.java
@@ -1,16 +1,21 @@
 package cmpt276.as3.assignment3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 import cmpt276.as3.assignment3.model.OptionsData;
 
@@ -19,7 +24,8 @@ import cmpt276.as3.assignment3.model.OptionsData;
  * and number of mines for the next game
  */
 public class OptionsActivity extends AppCompatActivity {
-
+    final int SIZE_OPTIONS = 3;
+    final int MINE_OPTIONS = 4;
     final String TAG = "TAG_MSG";
     private OptionsData optionsData = OptionsData.getInstance();
 
@@ -36,11 +42,19 @@ public class OptionsActivity extends AppCompatActivity {
         displayAllSpinners();
     }
 
+    /**
+     * Calling method to create both size and mineNum Spinner
+     */
     private void displayAllSpinners() {
         setUpSpinner(R.id.sizeSpinner, R.array.size);
         setUpSpinner(R.id.mineSpinner, R.array.mines);
     }
 
+    /**
+     * Create a Spinner and fill it using a string array in strings.xml
+     * @param spinnerId is the spinner to fill
+     * @param stringArrayId is the array of strings to fill the Spinner
+     */
     private void setUpSpinner(int spinnerId, int stringArrayId) {
         Spinner spinner = findViewById(spinnerId);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource
@@ -49,25 +63,42 @@ public class OptionsActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
     }
 
+    /**
+     * Return the index in the array of strings that matches with the String parameter
+     * @param arraySize
+     * @param arrayId is the array of strings to check with
+     * @param chosenItem is the String being checked
+     * @return
+     */
+    private int getStringArrayIdx(int arraySize, int arrayId, String chosenItem) {
+        int chosenIdx = 0;
+        for(int i = 0; i < arraySize; i++) {
+            String currOption = getResources().getStringArray(arrayId)[i];
+            if(chosenItem.equals(currOption)) {
+                chosenIdx = i;
+            }
+        }
+        return chosenIdx;
+    }
+
+    /**
+     * Calling method to use Singleton class' setters for both mines and size
+     */
     private void setOptions() {
         setSizeOptions();
         setMineOptions();
     }
 
+    /**
+     * Use the setters from the Singleton class to set the values for rows
+     * and columns depending on the Spinner option chosen
+     */
     private void setSizeOptions() {
-        final int SIZE_OPTIONS = 3;
         Spinner sizeSpinner = findViewById(R.id.sizeSpinner);
         String chosenSize = sizeSpinner.getSelectedItem().toString();
-        int chosenSizeIdx = 0;
+        int sizeIdx = getStringArrayIdx(SIZE_OPTIONS, R.array.size, chosenSize);
 
-        for(int i = 0; i < SIZE_OPTIONS; i++) {
-            String currSizeOption = getResources().getStringArray(R.array.size)[i];
-            if(chosenSize.equals(currSizeOption)) {
-                chosenSizeIdx = i;
-            }
-        }
-
-        switch(chosenSizeIdx) {
+        switch(sizeIdx) {
             case 0:
                 optionsData.setRowNum(4);
                 optionsData.setColumnNum(6);
@@ -86,20 +117,16 @@ public class OptionsActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Use the setters from the Singleton class to set the value for
+     * the number of mines depending on the Spinner option chosen
+     */
     private void setMineOptions() {
-        final int MINE_OPTIONS = 4;
         Spinner mineSpinner = findViewById(R.id.mineSpinner);
         String chosenMineNum = mineSpinner.getSelectedItem().toString();
-        int chosenMineNumIdx = 0;
+        int mineNumIdx = getStringArrayIdx(MINE_OPTIONS, R.array.mines, chosenMineNum);
 
-        for(int i = 0; i < MINE_OPTIONS; i++) {
-            String currMineNumOption = getResources().getStringArray(R.array.mines)[i];
-            if(chosenMineNum.equals(currMineNumOption)) {
-                chosenMineNumIdx = i;
-            }
-        }
-
-        switch(chosenMineNumIdx) {
+        switch(mineNumIdx) {
             case 0:
                 optionsData.setMineNum(6);
                 break;
@@ -118,6 +145,10 @@ public class OptionsActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     *  Call the setters of the Singleton class to save the options
+     *  when leaving the options activity
+     */
     @Override
     public void onBackPressed() {
         setOptions();
