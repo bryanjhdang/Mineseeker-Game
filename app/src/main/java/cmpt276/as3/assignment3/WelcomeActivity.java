639313718e771c8d6baf2a523622413f0899d9bb
Bry-D <bryandang10@gmail.com>
2021-10-17 20:57:38 -0700

    Add spinning cat head animation to welcome activity

--- app/src/main/java/cmpt276/as3/assignment3/WelcomeActivity.java
+++ app/src/main/java/cmpt276/as3/assignment3/WelcomeActivity.java
@@ -6,7 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Initial activity displayed at startup.
@@ -20,6 +24,7 @@ public class WelcomeActivity extends AppCompatActivity {
         removeInitialBars();
         setContentView(R.layout.activity_main);
 
+        rotateAllIcons();
         setMenuButton();
     }
 
@@ -37,5 +42,23 @@ public class WelcomeActivity extends AppCompatActivity {
             startActivity(i);
             finish();
         });
+    }
+
+    private void rotateAllIcons() {
+        rotateIcon(findViewById(R.id.catHead1), 360f);
+        rotateIcon(findViewById(R.id.catHead2), -360f);
+    }
+
+    // https://stackoverflow.com/questions/32641150/how-to-make-imageview-constantly-spin
+    private void rotateIcon(ImageView img, float deg) {
+        RotateAnimation rotateAnimation = new RotateAnimation(0, deg,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+
+        rotateAnimation.setInterpolator(new LinearInterpolator());
+        rotateAnimation.setDuration(1000);
+        rotateAnimation.setRepeatCount(Animation.INFINITE);
+
+        img.startAnimation(rotateAnimation);
     }
 }
