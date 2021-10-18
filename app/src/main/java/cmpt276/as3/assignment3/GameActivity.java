e6aa8f98517552eab72197fe39390e94e319ac24
Lynn Nguyen <nnn2@sfu.ca>
2021-10-18 01:40:08 -0700

    Update GameActivity.java with Dialog.
    Add popup screen after the player clicks on the last mine.

--- app/src/main/java/cmpt276/as3/assignment3/GameActivity.java
+++ app/src/main/java/cmpt276/as3/assignment3/GameActivity.java
@@ -91,6 +91,13 @@ public class GameActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         gridButtonClicked(FINAL_ROW, FINAL_COL);
+
+                        if (numCatsFound == numMines) {
+                            FragmentManager manager = getSupportFragmentManager();
+                            MessageFragment dialog = new MessageFragment();
+                            dialog.show(manager, "MessageDialog");
+                            Log.i("TAG","Just Showed the dialog.");
+                        }
                     }
                 });
 
